f6b0c8a8eff24888f164415062ebbd90cf28690a
jacobt35 <jacotayl@live.unc.edu>
2020-11-12 14:32:19 -0500

    added globalnews and localnews

--- client/src/pages/newsPage.js
+++ client/src/pages/newsPage.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import NewsCard from "../components/newscard"
 import axios from "axios";
-//import newsPageStyle from "../components/modules/newsPage.module.css";
+import newsPageStyle from "../components/modules/newsPage.module.css";
 const APIKEY = 'e1609839b7mshbeec556ba3a5b6dp1d7311jsn10f13f0e49bc';
 
 
@@ -10,7 +10,9 @@ export class News extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      news: []
+      localNews: [],
+      usaNews: [],
+      worldNews: [],
     }
   }
 
@@ -20,7 +22,7 @@ componentDidMount() {
     url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
     params: {
       pageSize: '10',
-      q: 'coronavirus',
+      q: 'coronavirus north carolina',
       autoCorrect: 'true',
       pageNumber: '1',
       toPublishedDate: 'null',
@@ -33,41 +35,140 @@ componentDidMount() {
     }
   }).then(response => {
     this.setState({
-      news: response.data.value
+      localNews: response.data.value
     })
-    console.log(this.state.news);
   });
-};
+
+  axios({
+    method: 'GET',
+    url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
+    params: {
+      pageSize: '10',
+      q: 'coronavirus united states',
+      autoCorrect: 'true',
+      pageNumber: '1',
+      toPublishedDate: 'null',
+      fromPublishedDate: 'null',
+      withThumbnails: 'true'
+    },
+    headers: {
+      'x-rapidapi-key': 'e1609839b7mshbeec556ba3a5b6dp1d7311jsn10f13f0e49bc',
+      'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+    }
+  }).then(response => {
+    this.setState({
+      usaNews: response.data.value
+    })
+  });
+
+  axios({
+    method: 'GET',
+    url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
+    params: {
+      pageSize: '10',
+      q: 'coronavirus global',
+      autoCorrect: 'true',
+      pageNumber: '1',
+      toPublishedDate: 'null',
+      fromPublishedDate: 'null',
+      withThumbnails: 'true'
+    },
+    headers: {
+      'x-rapidapi-key': 'e1609839b7mshbeec556ba3a5b6dp1d7311jsn10f13f0e49bc',
+      'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+    }
+  }).then(response => {
+    this.setState({
+      worldNews: response.data.value
+    })
+  });
+}
 
 
   render() {
 
-    let articles = this.state.news;
-    articles.forEach(article =>{
+    let ncArticles = this.state.localNews;
+    let usaArticles = this.state.usaNews;
+    const parser = new DOMParser();
+  
+    ncArticles.forEach(article =>{
       let descrip = this.formatDescripiton(article.description, 20);
       article.description = descrip + '...';
-      console.log(descrip);
     }
       )
+   usaArticles.forEach(article => {
+      let descrip = this.formatDescripiton(article.description, 20);
+      article.description = descrip + '...';
+    })
+
+    this.state.worldNews.forEach(article => {
+      let descrip = this.formatDescripiton(article.description, 20);
+      article.description = descrip + '...';
+    })
+    
+
     
     return (
-      <ul>
-      {this.state.news.map(article => 
+      <div>
+      <ul class="horizontal list">
+      <h1>Local News</h1>
+      {this.state.localNews.map(article => 
       <li><NewsCard
         image = {article.image.url}
         title = {article.title}
         date = {article.datePublished.splice}
         publisher = {article.provider.name}
         description = {article.description}
-        link = {article.url}
-        /></li>)}
+        link = {article.url}/> 
+        </li>)}
       </ul>
+
+        <h1>National News</h1>
+      <ul class="horizontal list">
+        {this.state.usaNews.map(article => 
+          <li><NewsCard
+          image = {article.image.url}
+          title = {article.title}
+          date = {article.datePublished.splice}
+          publisher = {article.provider.name}
+          description = {article.description}
+          link = {article.url}/> 
+          </li>)}
+      </ul>
+
+      <h1>Global News</h1>
+      <ul class="horizontal list">
+      {this.state.worldNews.map(article => 
+          <li><NewsCard
+          image = {article.image.url}
+          title = {article.title}
+          date = {article.datePublished.splice}
+          publisher = {article.provider.name}
+          description = {article.description}
+          link = {article.url}/> 
+          </li>)}
+          </ul>
+    </div>
     )
-  }
+        }
 
   formatDescripiton(description, n){ 
     return description.split(" ").splice(0, n).join(" ");
   }
 }
 
+/*
+ <ul>
+        {this.state.usaNews.forEach(article => 
+          <li><NewsCard
+          image = {article.image.url}
+          title = {article.title}
+          date = {article.datePublished.splice}
+          publisher = {article.provider.name}
+          description = {article.description}
+          link = {article.url}/> 
+          </li>)}
+        )
+      </ul>
+      */
 export default News;
