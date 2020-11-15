40d58474e8fb96fc1b7d6dbcc359c8437ce7e06c
Julia Johnson <juliajj2@yahoo.com>
2020-11-15 12:24:33 -0500

    changed maps to quiz in navbar, edited login styles

--- client/src/pages/guidelines.js
+++ client/src/pages/guidelines.js
@@ -20,7 +20,7 @@ export default () => {
 
             <h1 class="page-title">CDC Guidelines</h1>
 
-            <p className={infoboxStyle.cdcSource}>This information comes directly from the Centers for Disease Control and Prevention Website. For more information, go to            <Link to="https://www.cdc.gov/" class="underline">CDC.gov</Link></p>
+            <p className={infoboxStyle.cdcSource}>This information comes directly from the Centers for Disease Control and Prevention Website. For more information, go to <a target="_blank" href="https://www.cdc.gov/" class="underline">CDC.gov</a></p>
 
         <div className={infoboxStyle.masonrycontainer}>
             <div className={`${infoboxStyle.masonryitem} ${infoboxStyle.itemOne}`}>
