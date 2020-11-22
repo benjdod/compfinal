5f694539cfdfedb29fa6780ecac363b6d4c2526f
Rachel Tucker <rach1234@live.unc.edu>
2020-11-22 18:02:23 -0500

    about page fix

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -9,7 +9,9 @@ export default () => {
     const references = [
         'https://figshare.com/articles/COVID-19_Event_Risk_Assessment_Planner/11965533?file=21967428',
         'https://twitter.com/joshuasweitz/status/1237556232304508928/',
-        'https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)31142-9/fulltext'
+        'https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)31142-9/fulltext',
+        'https://docs.google.com/spreadsheets/d/16K1OQkLD4BjgBdO8ePj6ytf-RpPMlJ6aXFg3PrIQBbQ/edit#gid=519189277',
+        'https://github.com/nytimes/covid-19-data'
     ]
 
     const refElts = []
@@ -30,31 +32,28 @@ export default () => {
         )
     }
 
-
-    // TODO: this needs more info, or just to be rewritten. The wording 
-    // needs to be explicit about the data we're using, as well as the assumptions
-    // we make in our equation model. We also should say what the equation is?
-
     // to add citations, add the citation link to the references array, then call
     // makeInline with the citation's array index to make an inline ref.
 
-    // TODO: just a tip if you're working on this in VS Code, alt-z toggles word wrap
+    // just a tip if you're working on this in VS Code, alt-z toggles word wrap
     // that will make your time a lot easier...
     return (
         <PageFrame>
             <h3>Methods</h3>
-            <p>We based our risk caluclation method off of Joshua Weitz's similar equation.{makeInline(0,1)} We also factored in relevant metrics based off of recent studies on the effectiveness of mask wearing, social distancing, and event location.</p>
+            <p>The COVID Risk Assessor expands upon Joshua Weitz's risk calculation equation, using his method as a base for calculating the user’s risk. {makeInline(0,1)} Using his equation, the Risk Assessor also takes in the users’ inputted event details to give a personalized risk percentage based on mask wearing, social distancing, and event location. To factor these relevant metrics we took data from a variety of research papers.</p>
             <ul>
                 <li>Social distancing: Increases in social distance have been found to correspond with decreased risk. One meta-analysis found a 2x change in relative risk per meter{makeInline(2)}, but our equation uses a more conservative estimate since the data is not as "settled" </li>
-                <li>Mask wearing of user</li>
+                <li>Mask wearing: Taking research compiled by Univ. of Colorado Boulder Prof. Jose L. Jimenez, we were able to infer that the general population wearing masks reduces the risk of emission by 50%. By other’s wearing cloth masks, your risk of inhaling air transmitted COVID is 30%.{makeInline(3)}</li>
+                <li>Event location: Using The New York Time’s ongoing repository of data on coronavirus cases and deaths in the U.S.{makeInline(4)}, the COVID Risk Assessor takes in the number of infections in the user’s event’s area, separated by county, and uses that data to give the user a more personalized risk assessment.</li>
             </ul>
 
             <h3>Risk</h3>
-            <p>If you take a quiz, you might be surprised to find how low your risk is. But it is important to remember that our assesement model is at best a rough estimate which cannot possibly account for every aspect of the situation. Furthermore, there can be no guarantees of any health outcome in the event of contracting COVID-19, and the disease has a reputation as being unpredictable in who it affects adversely, even in different age brackets. Writ large: <em><strong>This quiz should not be used as an implied permission to do stupid things!</strong></em> If you are at all in doubt about the risk of any activity you are thinking of engaging in, it is always better to err on the side of caution, especially in the face of a deadly disease.</p>
+            <p>If you take a quiz, you might be surprised to find out how low your risk percentage is. But it is important to remember that our assessment model is at best a rough estimate which cannot possibly account for every aspect of the situation. Furthermore, there can be no guarantees of any health outcome in the event of contracting COVID-19, and the disease has a reputation as being unpredictable in who it affects adversely, even in different age brackets. <em><strong>This quiz should not be used as an implied permission to do stupid things!</strong></em> If you are at all in doubt about the risk of any activity you are thinking of engaging in, it is always better to err on the side of caution, especially in the face of a deadly disease.</p>
 
             <h3>Your Data</h3>
             <p>We store your data doubly encrypted, and we can't see it directly. When you log in, your browser is given a unique key which is used to decrypt your quizzes, but the server is unable to generate or decrypt your data without you.</p>
             {/* STYLE: fancy references, maybe italics? smushed together at bottom of page under content. */}
+            <h3>Sources</h3>
             <div>
                 {refElts}
             </div>
